7a73ec38c40248c34b79c0ee27d8c3ce939b97be
Dmitrii Leshchanov <leshok.dimkaa@gmail.com>
2022-10-22 19:43:31 +0300

    HW#9

--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";
+const hideShowBtn = document.querySelector(".hide-show-block-button");
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
+
+hideShowBtn.addEventListener("click", function() {
+    if(mainClass.classList.contains(HIDDEN)) {
+        showDetails();
+        hideShowBtn.innerHTML = "Hide";
+    } else {
+        hideDetails();
+        hideShowBtn.innerHTML = "Show";
+    }
+})
+
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on ", anchors[i]);
+        showDetails();
         setDetails(anchors[i]);
     })
+}
+function showDetails() {
+    mainClass.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
 }
