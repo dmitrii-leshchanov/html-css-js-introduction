f99ee7d04bc7e3500ecc847140a35e342b0282a8
Dmitrii Leshchanov <leshok.dimkaa@gmail.com>
2022-11-01 12:15:35 +0200

    hw#12

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-let number = 12345;
-function sumDigits(number) {
-    let sum = 0;
-    if(number < 0) {
-        number = -number;
-    }
-    for(i = 0; number != 0; i++) {
-        sum = number % 10 + sum;
-        number = Math.floor(number / 10);       
-    }
-    return sum;
+function fromNumberToString(number, base) {
+    let result = '';
+    let array = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    do {
+        let digit = number % base;
+        let index = digit;
+        result = array[index] + result;
+        number = Math.trunc(number/base);
+    } while (number != 0);
 
+    return result;
 }
-console.log(sumDigits(number));
+
+function fromStringToNumber(string, base) {
+    let result = parseInt(string, base);
+    return result;
+}
+
+console.log(fromNumberToString(900550, 36));
+console.log(fromNumberToString(46016237, 36));
+console.log(fromNumberToString(11483, 2));
+
+console.log(fromStringToNumber('JAVA', 36));
+console.log(fromStringToNumber('REACT', 36));
+console.log(fromStringToNumber('10110011011011', 2));
