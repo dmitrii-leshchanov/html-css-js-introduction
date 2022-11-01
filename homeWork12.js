08fd4bb0ef89719bd5f9bdc133afaad84c09437d
Dmitrii Leshchanov <leshok.dimkaa@gmail.com>
2022-11-01 12:59:17 +0200

    hw#12

--- homeWork12.js
+++ homeWork12.js
@@ -1,6 +1,7 @@
+let array = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 function fromNumberToString(number, base) {
     let result = '';
-    let array = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     do {
         let digit = number % base;
         let index = digit;
@@ -12,7 +13,12 @@ function fromNumberToString(number, base) {
 }
 
 function fromStringToNumber(string, base) {
-    let result = parseInt(string, base);
+    // let result = parseInt(string, base);
+    let result = 0;
+    for(let i = 0; i < string.length; i++) {
+        let code = array.findIndex(el => el == string[i]);
+        result = result * base + code;
+    }
     return result;
 }
 
