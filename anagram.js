3d08a922893df718a0ae587e593a6e3c3df33f65
Dmitrii Leshchanov <leshok.dimkaa@gmail.com>
2022-11-22 14:20:38 +0200

    HW #18

---
+++ anagram.js
@@ -0,0 +1,33 @@
+function isAnagram(str1, str2) {   
+    if(str1.length == str2.length) {
+        const str1Ar = Array.from(str1);
+        const str2Ar = Array.from(str2);
+        const occurences = {};
+        str1Ar.forEach(element => {
+            if(occurences[element]) {
+                occurences[element]++;
+            } else {
+                occurences[element] = 1;
+            }
+        })
+        let isTrue = true;
+        str2Ar.forEach((element, i) => {
+            if(occurences[str2Ar[i]]) {
+                occurences[element]--;          
+            } else {
+                occurences[element] = -1;
+                isTrue = false;
+                return isTrue;
+            }
+        })
+        
+        return isTrue;
+           
+    } else {
+        return false;
+    }
+}
+
+const word = "little";
+const result = isAnagram(word, "chop");
+console.log(result);
